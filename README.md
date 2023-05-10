bdd1c677745eb8744cac5f70e4e1505f98ebdf1e
Diana-Daniela <dianavdaniela@yahoo.com>
2023-05-10 11:45:40 +0300

    primul commit

---
+++ README.md
@@ -0,0 +1,25 @@
+UnitTesting Automation Framework
+
+Site tested: https://the-internet.herokuapp.com/
+Design pattern used: Unit Testing
+Methodology: framework
+To import project\
+git clone https://github.com/Diana-Daniela/Proiect_final_TA.git
+
+Libraries to install:\
+pip install -U selenium\
+pip install webdriver-manager
+
+Run tests:\
+click dreapta in zona de editare -> Run "nume fisier.py"
+sau click pe sageata verde din dreptul Clasei/metodei
+
+Troubleshoot:\
+a.\
+Daca nu merge cu pip incercati comanda: py -m pip install selenium\
+
+b.\
+Daca nici asa nu merge:\
+File -> Settings -> Click pe Project: [nume_proiect] -> Python Interpreter -> +\
+Cautati 'selenium' -> Install Package\
+La fel si pentru restul librariilor 
---
+++ Suite_teste.py
@@ -0,0 +1,134 @@
+import unittest
+from time import sleep
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+from webdriver_manager import chrome
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+
+class Teste_Proiect_Final(unittest.TestCase):
+    DIGEST_AUTHENTICATION = (By.LINK_TEXT, "Digest Authentication")
+    USERNAME = 'admin'
+    PASSWORD = 'admin'
+    AUTH_CONFIRM_MESSAGE = (By.TAG_NAME, 'p')
+    RESULT_MESSAGE = (By.ID, "result")
+    FORM_AUTHENTICATION = (By.LINK_TEXT, "Form Authentication")
+    ALERT_BUTTON = (By.XPATH, '//button[text()="Click for JS Alert"]')
+    CONFIRM_BUTTON = (By.XPATH, '//button[text()="Click for JS Confirm"]')
+    PROMPT_BUTTON = (By.XPATH, '//button[text()="Click for JS Prompt"]')
+    CONTEXT_BOX = (By.ID, "hot-spot")
+    LOGIN_BUTTON = (By.XPATH, "//i[@class='fa fa-2x fa-sign-in']")
+
+
+    def setUp(self):      #  aceasta metoda este obligatorie
+        self.driver = webdriver.Chrome()
+        self.driver.maximize_window()
+        self.driver.implicitly_wait(5)
+        sleep(2)
+        self.driver.get("https://the-internet.herokuapp.com/")  # linkul web se poate pune aici sau la fiecare test unitar de mai jos
+
+    def tearDown(self):
+        self.driver.quit()
+
+    def test_1_page_title_corect(self):
+        expected_title = "The Internet"
+        actual_title = self.driver.title
+        assert actual_title == expected_title, f"Titlu incorect: {actual_title}"
+        print("Titlul este corect")
+
+    # se valideaza mesajul de confirmare dupa introducerea corecta a userului si a parolei in meniul digest_auth
+    def test_2_mesaj_corect_user_pass_corecte(self):
+        self.driver.get("https://the-internet.herokuapp.com/")
+        self.driver.get(f'https://{self.USERNAME}:{self.PASSWORD}@the-internet.herokuapp.com/digest_auth')
+        sleep(3)
+        confirm_message = self.driver.find_element(*self.AUTH_CONFIRM_MESSAGE).text
+        self.assertEqual(confirm_message, "Congratulations! You must have the proper credentials.")
+
+    def test_3_atribut_href_corect(self):
+        actual_link = self.driver.find_element(By.XPATH, '//a[@href="http://elementalselenium.com/"]').get_attribute('href')
+        assert actual_link == 'http://elementalselenium.com/', 'Link-ul este gresit'
+        print('Link-ul verificat este corect')
+
+    def test_4_alert(self):
+        self.driver.find_element(*self.ALERT_BUTTON).click()
+        obj = self.driver.switch_to.alert # Ne-am mutat de pe pagina noastra pe fereastra de alerta si am salvat fereastra intro variabila obj
+        print(f"Mesajul de pe fereastra de alerta este: {obj.text}") # metoda text extrage textul din alerta din cazul nostru (din elementul obj)
+        obj.accept() # Dam OK la alerta noastra
+        sleep(2)
+        message = self.driver.find_element(*self.RESULT_MESSAGE).text
+        sleep(4)
+        self.assertEqual(message, "You successfully clicked an alert", "Mesajul nu este cel asteptat")
+
+    def test_5_confirm_ok(self):
+        self.driver.find_element(*self.CONFIRM_BUTTON).click()
+        obj = self.driver.switch_to.alert
+        print(f"Mesajul de pe fereastra este: {obj.text}")
+        obj.accept() # echivalent cu ok
+        message = self.driver.find_element(*self.RESULT_MESSAGE).text
+        sleep(2)
+        self.assertEqual(message,"You clicked: Ok","Mesajul nu este cel asteptat")
+
+    def test_6_confirm_cancel(self):
+        self.driver.find_element(*self.CONFIRM_BUTTON).click()
+        obj = self.driver.switch_to.alert
+        obj.dismiss()  ## cu dismiss dam cancel la pop-up de confirm alert
+        message = self.driver.find_element(*self.RESULT_MESSAGE).text
+        sleep(2)
+        self.assertEqual(message, "You clicked: Cancel", "Mesajul nu este cel asteptat")
+
+    def test_7_prompt(self):
+        self.driver.find_element(*self.PROMPT_BUTTON).click()
+        obj = self.driver.switch_to.alert
+        input_text = "Diana"
+        obj.send_keys(input_text) #### Trimitem textul Diana in text-field-ul promptului
+        sleep(4)
+        obj.accept()
+        sleep(3)
+        message = self.driver.find_element(*self.RESULT_MESSAGE).text
+        sleep(2)
+        self.assertEqual(message, f"You entered: {input_text}", "Mesajul nu este cel asteptat")
+
+    def test_8_promt_cancel(self):
+        self.driver.get("https://the-internet.herokuapp.com/javascript_alerts")
+        self.driver.maximize_window()
+        self.driver.find_element(*self.PROMPT_BUTTON).click()
+        obj = self.driver.switch_to.alert
+        obj.dismiss()  # cu dismiss dam cancel la pop-up de confirm alert
+        message = self.driver.find_element(*self.RESULT_MESSAGE).text
+        sleep(2)
+        self.assertEqual(message, "You entered: null", "Mesajul nu este cel asteptat")
+
+    def test_9_context_menu(self):
+        context_box = self.driver.find_element(*self.CONTEXT_BOX) # alerta apare doar in dreptunghiul respectiv
+        context_box.click() # folosind click() facem click stanga, click-ul normal
+        sleep(2)
+        action = ActionChains(self.driver)  # action chains ne ajutam sa facem click dreapta
+        action.context_click(context_box).perform() # cu metoda context_click accesam metoda perform() care e adevaratul click dreapta
+        sleep(3)
+        # ne-am mutat pe alerta care a aparut si am dat click pe OK
+        self.driver.switch_to.alert.accept()
+        sleep(2)
+
+    def test_10_url_login_auth_corect(self):
+        self.driver.find_element(*self.FORM_AUTHENTICATION).click()
+        expected_url = "https://the-internet.herokuapp.com/login"
+        actual_url = self.driver.current_url
+        assert actual_url == expected_url, f"URL incorect:{actual_url}"
+        print("Noul URL este corect")
+
+    def test_11_buton_login_displayed(self):
+        buton_login = self.driver.find_element(*self.LOGIN_BUTTON)
+        self.assertTrue(buton_login.is_displayed(), "Butonul de Login nu este afisat")
+        print("Butonul este afisat")
+
+    def test_12_element_xpath(self):
+        expected_text = "Login Page"
+        actual_text = self.driver.find_element(By.XPATH, "//h2").text
+        assert actual_text == expected_text, "Textul nu este cel asteptat"
+        print("Textul este corect")
+
+
